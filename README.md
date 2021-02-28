8f435fdf4e70b3db3a81cf803dff109e753dce39
rioj7 <38918937+rioj7@users.noreply.github.com>
2021-02-28 13:02:44 +0100

    v1.0.0 Initial version

--- README.md
+++ README.md
@@ -1,2 +1,189 @@
-# vscode-file-templates
-Visual Studio Code extension to create new files from User constructed templates.
+# File Templates
+
+Create files from defined templates. Templates can be defined at different levels: Extension, User, Workspace and Folder.
+
+Based on the extension [File Templates for VSCode](https://marketplace.visualstudio.com/items?itemName=bam.vscode-file-templates) by Venkatesh Boddu.
+
+This extension can be used as a replacement for **File Templates for VSCode**. It uses the same command names and key bindings.
+
+It fixes a number of problems and adds a few features.
+
+* command `template.newTemplateFromFile` is renamed to `templates.newTemplateFromFile`<br/>Change this if you have a custom key binding.
+* no restriction on naming the template
+* add a few variables for File Path and Custom Date, User input and Snippets
+
+## Features
+
+* Create new files from defined templates.
+* Create new File Templates either from an existing file or a blank file.
+* Edit User defined Templates.
+* Use [variables](#template-variables) for Author Name, Date, File Path, User input and Snippets.
+
+## Usage
+
+* If you select an entry in the File Explorer and use the Context menu (Right-click) you will see a menu entry **New File from Template**.<br/>
+  If the Explorer entry is a directory the new file will be created in this directory.<br/>
+  If the Explorer entry is a file the new file will a sibling.<br/>
+  It shows a menu with existing templates. Select the desired template and enter the new file name without extension. The new file will get the same extension as the selected template.
+* In the  Context menu (Right-click) of the editor you will see menu entries:
+    * **New File from Template** : Create a new file that is a sibling of the current file.
+    * **New Template from File** : Use the current active document as a start for a new template.<br/>You are asked in which directory you want this template stored and with which name.
+
+      ![Select Folder for New Template](images/select-folder-new-template.png)<br/>
+      _Theme: Light+_
+
+    * **Next Snippet in File** : the [`${snippet}` variables](#template-variables) needs to be evaluated after creation of the file because the editor needs to be put in _Snippet_ mode. This also resolves the `${cursor}` variable as a final snippet.<br/>If there are no `${snippet}` or `${cursor}` variables nothing happens.
+* From the Command Palette you can select:
+    * **Files: New File from Template** (`templates.newFileFromTemplate`) - to create a new file from a template
+    * **Files: New File Template** (`templates.newTemplate`) - to create a new blank file template.
+    * **Files: New Template from File** (`templates.newTemplateFromFile`) - to create a new template from the current active file.
+    * **Files: Edit File Template** (`templates.editTemplate`) - to edit a template.
+
+      ![Select Template to Edit](images/select-template-to-edit.png)<br/>
+      _Theme: Light+_
+
+    * **Files: Next Snippet in File** (`templates.nextSnippet`) - to evaluate the next `${snippet}` variable.
+* You can define [variables](#template-variables) in the templates for Author Name, Date, File Path, User input and Snippets
+
+If you use snippet variables often it might be handy to define a key binding for the command: `templates.nextSnippet`
+
+## Templates Location
+
+The templates can be stored in several locations:
+
+* **Extension** : The extension has a number of predefined templates that can not be edited or add to. After an update of the extension these edits would be gone.
+* **User** : in a user defined directory from the setting `templates.folder` in the User settings (a good location is `<homedir>/.vscode/templates`, the installed extensions are stored in a sibling directory) (Optional)
+* **Multiroot Workspace** : in a user defined directory from the setting `templates.folder` in the Workspace settings (`.code-workspace` file) (Optional)
+* **Worksapce/Folder** : When one folder is open the templates are stored in the directory `.vscode/templates`
+
+Each location is more specific of where the template can be used. Templates with the same name override templates on a more generic level. When you need to select a template to create from it shows the location of the template and the more specific ones are listed first
+
+A directory is only created when store a new template in that directory.
+
+## Template variables
+
+In the template you can define a number of variables. They are evaluated at file creation time except for `${snippet}`.
+
+A number of variables is identical to the [variables that can be used in `tasks.json` and `launch.json`](https://code.visualstudio.com/docs/editor/variables-reference#_predefined-variables):
+
+* `${relativeFile}` - the current opened file relative to workspaceFolder
+* `${relativeFileDirname}` - the current opened file's dirname relative to workspaceFolder
+* `${fileBasename}` - the current opened file's basename
+* `${fileBasenameNoExtension}` - the current opened file's basename with no file extension
+* `${fileExtname}` - the current opened file's extension
+
+The next use settings:
+
+* `${author}` : use the value for setting `templates.author`
+* `${dateTimeFormat}` : use the setting `templates.dateTimeFormat` to construct a [date-time](#variable-datetimeformat).
+* `${date}` : show the current date and time in a fixed format, for historic reasons this variable is still allowed.
+
+The next variables need additional information. This is part of the variable and needs to be specified using separator strings.
+
+<code>${<em>variableName</em> <em>separator</em> <em>properties</em> <em>separator</em>}</code>
+
+All _`separator`_'s used in a variable need to be the same.
+
+The _`separator`_ is a string of 1 or more characters that are not part of the a to z alfabet, `$` or `{}`, in regular expression `[^a-zA-Z{}$]+`. Choose a character string that is not used in the  _`properties`_ part. If you need to use more than 1 character be carefull if you use the same character, if you experience unwanted behavior. The reason is that JavaScript regular expression does not have non-backtrack greedy quantifiers. Currently the variable is matched with 1 regular expression. This makes everything easy to implement.
+
+* `${input:description:}` : Ask the user some test and use the _`properties`_ part as the description for the InputBox<br/>
+  Example: `${input:Title of this page:}`
+* `${snippet:definition:}` : you can use the full syntax of the [Visual Studio Code snippets](https://code.visualstudio.com/docs/editor/userdefinedsnippets#_snippet-syntax).<br/>A snippet is evaluated after the file is created with the command: **Next Snippet in File** from the Context Menu or Command Palette. The editor needs to be put in _Snippet_ mode. Apply this command for every `${snippet}` or `${cursor}` variable still in the file.<br/>
+  Example: `${snippet##${1|*,**,***|} ${TM_FILENAME/(.*)/${1:/upcase}/} ${1}##}`
+
+If a snippet does not need user interaction (variable transforms) it could be done at file creation but determining this automatic might be a ToDo item.
+
+A final empty variable to place the cursor:
+
+* `${cursor}` : If you want the cursor to end in a specific location after the file is created or as a last snippet use this variable
+
+## Variable dateTimeFormat
+
+This variable `${dateTimeFormat}` uses the setting `templates.dateTimeFormat`.
+
+The setting `templates.dateTimeFormat` is an object with properties that are use to call [`Intl.DateTimeFormat`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/DateTimeFormat) to create a language-sensitive format of the current date and time.
+
+The `locale` and `options` properties are the arguments for the [`Intl.DateTimeFormat` constructor](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/DateTimeFormat/DateTimeFormat) and are optional.
+
+The `locale` property can be a single string or an array of strings of language tags. If not specified the browser default locale is used.
+
+The `template` property is an optional template string that uses the same placeholder syntax as the [Javascript template strings](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals). You can add as many literal text as needed.
+
+The only expressions valid are the `type` values returned by the [`Intl.DateTimeFormat.prototype.formatToParts()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/DateTimeFormat/formatToParts) method. See first example.
+
+If there is no `template` property the value parts of the `Intl.DateTimeFormat.prototype.formatToParts()` are joined. See second example.
+
+### Example 1
+
+```json
+    "templates.dateTimeFormat": {
+      "locale": "en-US",
+      "options": {
+        "year": "numeric",
+        "month": "2-digit",
+        "day": "2-digit",
+        "hour12": false,
+        "hour": "2-digit",
+        "minute": "2-digit",
+        "second": "2-digit"
+      },
+      "template": "${year}/${month}/${day}-${hour}:${minute}:${second}"
+    }
+```
+
+The result is
+
+```
+2020/03/19-18:01:18
+```
+
+### Example 2
+
+You can use a different locale and number system and use the long format:
+
+```json
+    "templates.dateTimeFormat": {
+      "locale": "fr-FR-u-nu-deva",
+      "options": {
+        "dateStyle": "full",
+        "timeStyle": "full"
+      }
+    }
+```
+
+The result is
+
+```
+jeudi १९ mars २०२० à १७:५९:५७ heure normale d’Europe centrale
+```
+
+## Extension Settings
+
+This extension has the following settings:
+
+* `templates.author` : Set the Author name.
+* `templates.Author` : Set the Author name. **Deprecated**: Please use `templates.author` instead.
+* `templates.folder` : Define a File system path for a directory to save the templates at this level. Can only be defined at User and Multiroot Workspace level.
+* `templates.dateTimeFormat` : An object describing the properties used by the [`${dateTimeFormat}` variable](#variable-datetimeformat)
+
+## KeyBindings
+
+* Create a New File from template
+
+    * Windows:  <kbd>Ctrl</kbd>+<kbd>N</kbd>
+    * MAC: <kbd>Cmd</kbd>+<kbd>N</kbd>
+
+* New Untitled file (Visual Studio Code command: `workbench.action.files.newUntitledFile`)
+
+    * Windows:  <kbd>Ctrl</kbd>+<kbd>Alt</kbd>+<kbd>N</kbd>
+    * MAC: <kbd>Cmd</kbd>+<kbd>Alt</kbd>+<kbd>N</kbd>
+
+## TODO
+
+* support multiple `${cursor}` variables. To get a Multi Cursor after creating a file from a template
+
+## Release Notes
+
+### 1.0.0
+
+Initial release of File Templates
---
+++ vscode-file-templates.js
@@ -0,0 +1,339 @@
+const vscode = require('vscode');
+const path = require('path');
+const fs = require('fs');
+
+const extensionTemplatesPath = path.join(__dirname, 'templates');
+
+const getProperty = (obj, prop, deflt) => { return obj.hasOwnProperty(prop) ? obj[prop] : deflt; };
+
+const workspaceFolder2TemplateDirFolder = wsf => ({ label: `  $(folder) Folder: ${wsf.name}`, uri: vscode.Uri.file(path.join(wsf.uri.fsPath, '.vscode', 'templates')) });
+
+function pathExists(path) { return fs.existsSync(path); }
+function pathIsDirectory(path) { return pathExists(path) && fs.statSync(path).isDirectory(); }
+function pathIsFile(path) { return pathExists(path) && fs.statSync(path).isFile(); }
+function createDirectory(path) { if (!pathExists(path)) { fs.mkdirSync(path, { recursive: true } ); } }
+function removeThemeIcon(text) { return text.replace(/\$\([-\w]+\)/g, ''); }
+const nonPosixPathRegEx = new RegExp('^/([a-zA-Z]):/');
+const lowerCaseDriveLetter = p => p.replace(nonPosixPathRegEx, match => match.toLowerCase() );
+
+function withTemplateDirs(action) {
+  if (vscode.workspace.workspaceFolders === undefined) {
+    vscode.window.showErrorMessage('No workspace open');
+    return;
+  }
+  let config = vscode.workspace.getConfiguration('templates', vscode.workspace.workspaceFolders[0].uri);
+  let inspect = config.inspect('folder') || {};
+  let templateDirs = {};
+  if (inspect.globalValue) {
+    templateDirs.global = { label: '  $(home) User', uri: vscode.Uri.file(inspect.globalValue) }
+  }
+  if ((vscode.workspace.workspaceFolders.length > 1) && inspect.workspaceValue) {
+    templateDirs.workspace = { label: `  $(list-tree) Workspace: ${vscode.workspace.name.replace(' (Workspace)', '')} `, uri: vscode.Uri.file(inspect.workspaceValue) }
+  }
+  templateDirs.folders = vscode.workspace.workspaceFolders.map( wsf => workspaceFolder2TemplateDirFolder(wsf) );
+  action(templateDirs);
+}
+
+function getTemplates(templateDirs, override = false) {
+  return new Promise( (resolve) => {
+    let templates = [];
+    let updateTemplates = (templateDir, sequence) => {
+      if (!templateDir) { return; }
+      let dirPath = templateDir.uri.fsPath;
+      if (!pathIsDirectory(dirPath)) { return; }
+      let files = fs.readdirSync(dirPath);
+      files.forEach((file, index) => {
+        if (file === 'file-variables.txt') { index = 10000; } // when will get an issue by somebody who has that many templates defined
+        if (override) { templates = templates.filter( t => t.label !== file); }
+        templates.push( {label: file, description: templateDir.label, filePath: path.join(dirPath, file), sort: [sequence, index] } );
+      });
+    };
+    let numFolders = templateDirs.folders.length;
+    updateTemplates(templateDirs.extension, 2 + numFolders);
+    updateTemplates(templateDirs.global, 1 + numFolders);
+    updateTemplates(templateDirs.workspace, 0 + numFolders);
+    templateDirs.folders.forEach( updateTemplates );
+    templates.sort( (a,b) => {
+      if ( a.sort[0] < b.sort[0]) return -1;
+      if ( a.sort[0] > b.sort[0]) return 1;
+      return a.sort[1] - b.sort[1];
+    });
+    resolve(templates);
+  });
+}
+
+function editTemplate() {
+  withTemplateDirs( templateDirs => {
+    getTemplates(templateDirs)
+      .then(templatesInfo => {
+        if (templatesInfo.length === 0) {
+          vscode.window.showInformationMessage('No templates found.');
+          return;
+        }
+        return vscode.window.showQuickPick(templatesInfo, { placeHolder: 'Select a template to edit' });
+      })
+      .then(templateInfo => {
+        if (!templateInfo) { return; }
+        vscode.commands.executeCommand('vscode.open', vscode.Uri.file(templateInfo.filePath));
+      });
+  });
+}
+
+const NEW_FILE = 'Files: New File';
+// const CREATE_TEMPLATE = 'Files: New File Template';
+// const EDIT_TEMPLATE = 'Files: Edit File Template';
+// const TEMPLATES_PREFIX = 'Template: ';
+const cursorVar = '${cursor}';
+
+// String.prototype.replaceAll = function(search, replacement) {
+//   return this.replace(new RegExp(search, 'g'), replacement);
+// };
+
+const dateTimeFormat = (args) => {
+  let locale = getProperty(args, 'locale', undefined);
+  let options = getProperty(args, 'options', undefined);
+  let template = getProperty(args, 'template', undefined);
+  let parts = new Intl.DateTimeFormat(locale, options).formatToParts(new Date());
+  if (!template) { return parts.map(({type, value}) => value).join(''); }
+  let dateTimeFormatParts = {};
+  parts.forEach(({type, value}) => { dateTimeFormatParts[type] = value; });
+  return template.replace(/\$\{(\w+)\}/g, (match, p1) => { return getProperty(dateTimeFormatParts, p1, ''); });
+};
+
+function getAuthor(config) {
+  let author = config.get('Author');
+  if (author !== 'Author') return author;  // still using the old name
+  return config.get('author');
+}
+
+function getVariableWithParamsRegex(varName) { return new RegExp(`\\$\\{${varName}(\\}|([^a-zA-Z{}$]+)(.+?)\\2\\})`); }
+
+async function gotoCursor(editor, offsetCursor = -1) {
+  let document = editor.document;
+  if (offsetCursor === -1) {
+    offsetCursor = document.getText().indexOf(cursorVar);
+    if (offsetCursor >= 0) {
+      let start = document.positionAt(offsetCursor);
+      await editor.edit( editBuilder => { editBuilder.replace(new vscode.Range(start, start.translate(0, cursorVar.length)), ''); });
+    }
+  }
+  if (offsetCursor >= 0) {
+    let start = document.positionAt(offsetCursor);
+    editor.selections = [ new vscode.Selection(start,start) ];
+  }
+}
+
+function createFile(filepath, data = '', fileExtname = '') {
+  vscode.window.showInputBox({ prompt: 'Enter new file name' + (fileExtname.length !==0 ? ' (without extension)' : '') })
+    .then(fileBasenameNoExtension => {
+      if (!fileBasenameNoExtension) { return; }
+      let fileBasename = fileBasenameNoExtension + fileExtname;
+      let curDir = filepath;
+      if (!pathIsDirectory(filepath))
+        curDir = path.dirname(filepath);
+
+      let newFilePath = path.join(curDir, fileBasename);
+      if (pathIsFile(newFilePath)) {
+        vscode.window.showErrorMessage(`File already exists: ${newFilePath}`);
+        return;
+      }
+      let newFileURI = vscode.Uri.file(newFilePath);
+      let workspaceURI = vscode.workspace.getWorkspaceFolder(newFileURI).uri; // should always have a result
+      let relativeFile = 'Unknown';
+      let relativeFileDirname = relativeFile;
+      if (lowerCaseDriveLetter(newFileURI.path).indexOf(lowerCaseDriveLetter(workspaceURI.path)) === 0) { relativeFile = newFileURI.path.substring(workspaceURI.path.length+1); }
+      let pos = relativeFile.lastIndexOf(fileBasename);
+      if (pos !== -1) { relativeFileDirname = relativeFile.substring(0, pos); }
+      if (relativeFileDirname.endsWith('/')) { relativeFileDirname = relativeFileDirname.substring(0, relativeFileDirname.length-1); }
+
+      let config = vscode.workspace.getConfiguration('templates', newFileURI);
+      data = data.replace(/\$\{author\}/ig, getAuthor(config));  // config.get('author'));
+      data = data.replace(/\$\{date\}/ig, new Date().toDateString());
+      data = data.replace(/\$\{dateTimeFormat\}/g, dateTimeFormat(config.get('dateTimeFormat')));
+      data = data.replace(/\$\{fileBasename\}/g, fileBasename);
+      data = data.replace(/\$\{fileBasenameNoExtension\}/g, fileBasenameNoExtension);
+      data = data.replace(/\$\{fileExtname\}/g, fileExtname);
+      data = data.replace(/\$\{relativeFile\}/g, relativeFile);
+      data = data.replace(/\$\{relativeFileDirname\}/g, relativeFileDirname);
+      // for historic reasons
+      data = data.replace(/\$\{file\}/ig, fileBasename);
+
+      let offsetCursor = -1;
+      if (!getVariableWithParamsRegex('(?:input|snippet)').test(data)) {
+        offsetCursor = data.indexOf(cursorVar);
+        data = data.replace(/\$\{cursor\}/g, '');
+      }
+
+      fs.writeFile(newFilePath, data, (err) => {
+        if (err) {
+          vscode.window.showErrorMessage(`Cannot create new file: ${newFilePath}`);
+          return;
+        }
+        vscode.commands.executeCommand('vscode.open', newFileURI)
+          .then( async () => {
+            let editor = vscode.window.activeTextEditor;
+            if (!editor) { return; }
+            let document = editor.document;
+            if (lowerCaseDriveLetter(document.uri.path) !== lowerCaseDriveLetter(newFileURI.path)) { return; }
+
+            let inputVarRegEx = getVariableWithParamsRegex('input');
+            while (true) {
+              let found = document.getText().match(inputVarRegEx);
+              if (!found) { break; }
+              let start = document.positionAt(found.index);
+              let varRange = new vscode.Range(start, start.translate(0, found[0].length));
+              let input = await vscode.window.showInputBox({ prompt: found[3] });
+              if (input === undefined) { break; }
+              await editor.edit( editBuilder => { editBuilder.replace(varRange, input); });
+            }
+            if (getVariableWithParamsRegex('snippet').test(document.getText())) { return; }
+            gotoCursor(editor, offsetCursor);
+          });
+      });
+    });
+}
+
+function nextSnippet(editor, edit, args) {
+  let document = editor.document;
+  let found = document.getText().match(getVariableWithParamsRegex('snippet'));
+  if (found) {
+    let start = document.positionAt(found.index);
+    let varRange = new vscode.Range(start, start.translate(0, found[0].length));
+    return editor.insertSnippet(new vscode.SnippetString(found[3]), varRange);
+  }
+  return gotoCursor(editor);
+}
+
+var getWorkspaceFolder = async (folders) => {
+  if (folders.length === 1) { return folders[0]; }
+  return vscode.window.showQuickPick(folders, { placeHolder: 'Select a folder to place file' })
+    .then( folder => {
+      if (!folder) return undefined;
+      return folder;
+    });
+};
+
+async function getCurrentPath_TemplateDirFolder(uri, templateDirs) {
+  let currentURI = uri;
+  if (!currentURI) {
+    let editor = vscode.window.activeTextEditor;
+    if (editor) { currentURI = editor.document.uri; }
+  }
+  if (!currentURI) {
+    let templateDirFolder = await getWorkspaceFolder(templateDirs.folders);
+    if (templateDirFolder) { currentURI = vscode.Uri.file(path.dirname(path.dirname(templateDirFolder.uri.fsPath))); }  // remove '/.vscode/templates' OS independent
+  }
+  let currentPath = undefined;
+  let templateDirFolder = undefined;
+  if (currentURI) {
+    let workspace = vscode.workspace.getWorkspaceFolder(currentURI);
+    if (workspace) {
+      currentPath = currentURI.fsPath;
+      templateDirFolder = workspaceFolder2TemplateDirFolder(workspace);
+    } else {
+      vscode.window.showErrorMessage('Current file not part of workspace.');
+    }
+  }
+  return {currentPath, templateDirFolder};
+  // TODO : create untitled file from template
+}
+
+async function newFileFromTemplate(uri) {
+  withTemplateDirs(async (templateDirs) => {
+    let {currentPath, templateDirFolder} = await getCurrentPath_TemplateDirFolder(uri, templateDirs);
+    if (!currentPath) return;
+    templateDirs.extension = { label: '  $(extensions) Extension', uri: vscode.Uri.file(extensionTemplatesPath) };
+    templateDirs.folders = [ templateDirFolder ];
+    let override = true;
+    getTemplates(templateDirs, override)
+      .then(templatesInfo => {
+        templatesInfo.unshift( { label: NEW_FILE } );
+        // [{label:CREATE_TEMPLATE}, {label:EDIT_TEMPLATE}]
+        return vscode.window.showQuickPick(templatesInfo, { placeHolder: 'Select a template to create from' });
+      })
+      .then(templateInfo => {
+        if (!templateInfo) { return; }
+
+        if (!templateInfo.filePath) {
+          // if(templateInfo.label === CREATE_TEMPLATE)
+          //   newTemplate();
+          // else if(templateInfo.label === EDIT_TEMPLATE){
+          //   editTemplate();
+          // }
+          if (templateInfo.label === NEW_FILE)
+            createFile(currentPath);
+          return;
+        }
+
+        fs.readFile(templateInfo.filePath, 'utf8', (err, data) => {
+          if (err) {
+            vscode.window.showErrorMessage('Cannot find the template');
+            return;
+          }
+          let extension = templateInfo.filePath.substring(templateInfo.filePath.lastIndexOf('.'));
+          createFile(currentPath, data, extension);
+        });
+      });
+  });
+}
+
+function createTemplate(templateText = '') {
+  withTemplateDirs( templateDirs => {
+    let folders = [...templateDirs.folders];
+    if (templateDirs.workspace) { folders.push(templateDirs.workspace); }
+    if (templateDirs.global)    { folders.push(templateDirs.global); }
+    vscode.window.showQuickPick(folders, { placeHolder: 'Select a folder to place template' })
+      .then( folder => {
+        if (!folder) { return };
+        vscode.window.showInputBox({ prompt: 'Enter template name with extension. (Ex: javascript.js)' })
+          .then(templateName => {
+            if (!templateName) { return; }
+            getTemplates( {folders: [folder]} ).then(templatesInfo => {
+              if (templatesInfo.find(t => t.label === templateName) !== undefined) {
+                vscode.window.showErrorMessage(`${removeThemeIcon(folder.label)}: Template \"${templateName}\" already exists.`);
+                return;
+              }
+              let templateDirPath = folder.uri.fsPath;
+              createDirectory(templateDirPath);
+              if (!pathIsDirectory(templateDirPath)) {
+                vscode.window.showErrorMessage(`Unable to create directory or path is not a directory: ${templateDirPath}`);
+                return;
+              }
+              let templatePath = path.join(templateDirPath, templateName);
+              fs.writeFile(templatePath, templateText, (err) => {
+                if (err) {
+                  vscode.window.showErrorMessage(err);
+                  return;
+                }
+                vscode.window.showInformationMessage(`${removeThemeIcon(folder.label)}: ${templateName} created.`);
+                vscode.commands.executeCommand('vscode.open', vscode.Uri.file(templatePath));
+              });
+            });
+          });
+      });
+  });
+}
+
+function newTemplateFromFile() {
+  let editor = vscode.window.activeTextEditor;
+  createTemplate(editor.document.getText());
+}
+
+function newTemplate() {
+  createTemplate();
+}
+
+function activate(context) {
+  context.subscriptions.push(vscode.commands.registerCommand('templates.newTemplateFromFile', newTemplateFromFile));
+  context.subscriptions.push(vscode.commands.registerCommand('templates.newTemplate', newTemplate));
+  context.subscriptions.push(vscode.commands.registerCommand('templates.editTemplate', editTemplate));
+  context.subscriptions.push(vscode.commands.registerCommand('templates.newFileFromTemplate', newFileFromTemplate));
+  context.subscriptions.push(vscode.commands.registerTextEditorCommand('templates.nextSnippet', nextSnippet));
+}
+
+function deactivate() { }
+
+module.exports = {
+  activate,
+  deactivate
+}
